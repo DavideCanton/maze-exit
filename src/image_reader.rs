2cb2ab52ee11497f7fe185bd1c5c655c29558404
Davide Canton <davide.canton5@gmail.com>
2021-05-02 16:32:45 +0200

    better color detection

--- src/image_reader.rs
+++ src/image_reader.rs
@@ -2,11 +2,13 @@ use std::path::Path;
 
 use image::error::ImageResult;
 use image::io::Reader as ImageReader;
-use image::GenericImageView;
+use image::{GenericImageView, Rgba};
 
 use crate::maze::Maze;
 use crate::maze_builder::MazeBuilder;
 use crate::position::Pos;
+
+const THRESHOLD: f64 = 250.0;
 
 pub fn read_from_image(path: &Path) -> ImageResult<Maze> {
     let image = ImageReader::open(path)?.decode()?;
@@ -18,14 +20,67 @@ pub fn read_from_image(path: &Path) -> ImageResult<Maze> {
             let p = image.get_pixel(x, y);
             let pos = Pos::new(x as i32, y as i32);
 
-            match p.0 {
-                [0, 0, 0, _] => builder = builder.add_wall(pos),
-                [255, 0, 0, _] => builder = builder.start(pos),
-                [0, 255, 0, _] => builder = builder.goal(pos),
-                _ => {}
+            if is_wall(p) {
+                builder = builder.add_wall(pos);
+            } else if is_start(p) {
+                builder = builder.start(pos);
+            } else if is_goal(p) {
+                builder = builder.goal(pos);
             }
         }
     }
 
     Ok(builder.build().unwrap())
+}
+
+fn is_goal(pixel: Rgba<u8>) -> bool {
+    color_difference(pixel.0, [0, 255, 0, 0]) < THRESHOLD
+}
+
+fn is_start(pixel: Rgba<u8>) -> bool {
+    color_difference(pixel.0, [255, 0, 0, 0]) < THRESHOLD
+}
+
+fn is_wall(pixel: Rgba<u8>) -> bool {
+    color_difference(pixel.0, [0, 0, 0, 0]) < THRESHOLD
+}
+
+fn color_difference(p1: [u8; 4], p2: [u8; 4]) -> f64 {
+    let [r1, g1, b1, _] = p1;
+    let [r2, g2, b2, _] = p2;
+    let r_mean = (r1 as f64 + r2 as f64) / 2.0;
+
+    let r = r1 as f64 - r2 as f64;
+    let g = g1 as f64 - g2 as f64;
+    let b = b1 as f64 - b2 as f64;
+
+    let r_sq = r * r;
+    let g_sq = g * g;
+    let b_sq = b * b;
+
+    let r_coeff = 2.0 + r_mean / 256.0;
+    let g_coeff = 4.0;
+    let b_coeff = 2.0 + 255.0 / 256.0 - r_mean / 256.0;
+
+    (r_coeff * r_sq + g_coeff * g_sq + b_coeff * b_sq).sqrt()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn detect_red() {
+        let pixel = Rgba::from([237u8, 28, 36, 0]);
+        assert!(is_start(pixel));
+
+        let pixel = Rgba::from([10u8, 28, 36, 0]);
+        assert!(!is_start(pixel));
+    }
+
+    #[test]
+    fn detect_green() {
+        let pixel = Rgba::from([34u8, 177, 76, 0]);
+        assert!(is_goal(pixel));
+    }
 }
