739be12718395e87695532e0a0b5700e4dce7763
Davide Canton <davide.canton5@gmail.com>
2022-04-16 15:24:02 +0200

    terminal displayer and env

--- maze_exit_bin/src/display/term_displayer.rs
+++ maze_exit_bin/src/display/term_displayer.rs
@@ -1,71 +1,121 @@
-use std::{collections::BinaryHeap, io::Write};
+use std::{
+    collections::BinaryHeap,
+    io::{stdout, Write},
+    thread::sleep,
+    time::Duration,
+};
 
+use crossterm::{
+    cursor::MoveTo,
+    queue,
+    style::{Color, Print, ResetColor, SetForegroundColor},
+    terminal::{Clear, ClearType},
+};
 use maze_exit_lib::{algorithm::QueueNode, generator::PathRef, maze::Maze, position::Pos};
 
 use crate::display::display_trait::Displayer;
 
-pub struct TerminalDisplayer<W: Write> {
-    writer: W,
+type CTResult = crossterm::Result<()>;
+
+pub struct TerminalDisplayer {
+    wall_char: char,
+    queue_char: char,
+    path_char: char,
+    start_char: char,
+    goal_char: char,
+    sleep_ms: Option<u64>,
 }
 
-impl<W: Write> TerminalDisplayer<W> {
-    pub fn new(writer: W) -> Self {
-        TerminalDisplayer { writer }
+impl TerminalDisplayer {
+    pub fn new(
+        wall_char: char,
+        queue_char: char,
+        path_char: char,
+        start_char: char,
+        goal_char: char,
+        sleep_ms: Option<u64>,
+    ) -> Self {
+        TerminalDisplayer {
+            goal_char,
+            path_char,
+            queue_char,
+            start_char,
+            wall_char,
+            sleep_ms,
+        }
     }
 
-    fn build_maze(
-        &self,
+    fn inner_display_image(
+        &mut self,
         maze: &Maze,
+        start_to_goal: f64,
         path: Option<PathRef>,
         queue: Option<&BinaryHeap<QueueNode<Pos>>>,
-    ) -> String {
-        let mut rows = Vec::with_capacity(maze.height() as usize);
+    ) -> CTResult {
+        let mut stdout = stdout();
 
-        for _ in 0..maze.height() {
-            let row = vec![" "; maze.width() as usize];
-            rows.push(row);
-        }
+        queue!(stdout, Clear(ClearType::All), MoveTo(0, 0), ResetColor,)?;
 
         for w in maze.walls() {
             let Pos { x, y } = w;
-            rows[*y as usize][*x as usize] = "#";
+            queue!(stdout, MoveTo(*x as u16, *y as u16), Print(self.wall_char))?;
         }
 
         if let Some(queue) = queue {
             for p in queue {
                 let Pos { x, y } = p.node;
-                rows[y as usize][x as usize] = ":";
+                let h: f64 = p.heuristic;
+                let v = (h / start_to_goal * 255.0) as u8;
+                queue!(
+                    stdout,
+                    MoveTo(x as u16, y as u16),
+                    SetForegroundColor(Color::Rgb {
+                        r: v,
+                        g: 255 - v,
+                        b: 0
+                    }),
+                    Print(self.queue_char)
+                )?;
             }
         }
 
         if let Some(path) = path {
+            queue!(stdout, SetForegroundColor(Color::Blue))?;
             for p in path {
                 let Pos { x, y } = p;
-                rows[*y as usize][*x as usize] = ".";
+                queue!(stdout, MoveTo(*x as u16, *y as u16), Print(self.path_char))?;
             }
         }
 
         let Pos { x, y } = maze.start;
-        rows[y as usize][x as usize] = "S";
-
+        queue!(
+            stdout,
+            MoveTo(x as u16, y as u16),
+            SetForegroundColor(Color::Red),
+            Print(self.start_char)
+        )?;
         let Pos { x, y } = maze.goal;
-        rows[y as usize][x as usize] = "G";
+        queue!(
+            stdout,
+            MoveTo(x as u16, y as u16),
+            SetForegroundColor(Color::Green),
+            Print(self.goal_char)
+        )?;
 
-        rows.into_iter()
-            .map(|r| r.join(""))
-            .collect::<Vec<String>>()
-            .join("\n")
-    }
+        queue!(stdout, MoveTo(0, maze.height() as u16 + 1), ResetColor,)?;
 
-    fn write(&mut self, s: String) -> Result<(), String> {
-        self.writer
-            .write_all(s.as_bytes())
-            .map(|_| ())
-            .map_err(|e| e.to_string())
+        stdout.flush()?;
+
+        self.sleep_ms
+            .map(Duration::from_millis)
+            .into_iter()
+            .for_each(sleep);
+
+        Ok(())
     }
 }
 
-impl<W: Write> Displayer for TerminalDisplayer<W> {
+impl Displayer for TerminalDisplayer {
     fn init(&mut self) -> Result<(), String> {
         Ok(())
     }
@@ -73,16 +123,21 @@ impl<W: Write> Displayer for TerminalDisplayer<W> {
     fn display_image(
         &mut self,
         maze: &Maze,
-        _start_to_goal: f64,
+        start_to_goal: f64,
         path: Option<PathRef>,
         queue: Option<&BinaryHeap<QueueNode<Pos>>>,
     ) -> Result<(), String> {
-        let s = self.build_maze(maze, path, queue);
-        self.write(s)?;
-        self.write("-".repeat(100))
+        self.inner_display_image(maze, start_to_goal, path, queue)
+            .map_err(|e| e.to_string())
     }
 
     fn wait_for_end(&self) -> Result<(), String> {
         Ok(())
+    }
+}
+
+impl Default for TerminalDisplayer {
+    fn default() -> Self {
+        TerminalDisplayer::new('+', '*', '*', 'S', 'G', Some(100))
     }
 }
--- maze_exit_bin/src/main.rs
+++ maze_exit_bin/src/main.rs
@@ -1,24 +1,68 @@
-use std::env;
-use std::error::Error;
-use std::io::stdout;
-use std::path::{Path, PathBuf};
-use std::rc::Rc;
-use std::time::Instant;
+use std::{
+    env::{self, VarError},
+    error::Error,
+    fmt::Display,
+    path::{Path, PathBuf},
+    rc::Rc,
+    time::Instant,
+};
 
+use enum_derive::ParseEnumError;
 use image::ImageResult;
 
-use crate::image_reader::{MazeImageReader, MazeReader};
-use maze_exit_lib::algorithm::a_star;
-use maze_exit_lib::generator::{JpsGenerator, MazeChildrenGenerator};
-use maze_exit_lib::heuristics::{DiagonalHeuristic, HeuristicFn, MazeHeuristic};
-use maze_exit_lib::maze::Maze;
+use maze_exit_lib::{
+    algorithm::a_star,
+    generator::{JpsGenerator, MazeChildrenGenerator},
+    heuristics::{DiagonalHeuristic, HeuristicFn, MazeHeuristic},
+    maze::Maze,
+};
 
-use crate::display::{
-    display_trait::Displayer, gui_displayer::GuiDisplayer, term_displayer::TerminalDisplayer,
+use crate::{
+    display::{
+        display_trait::Displayer, gui_displayer::GuiDisplayer, term_displayer::TerminalDisplayer,
+    },
+    image_reader::{MazeImageReader, MazeReader},
 };
 
 mod display;
 mod image_reader;
+
+#[macro_use]
+extern crate custom_derive;
+#[macro_use]
+extern crate enum_derive;
+
+custom_derive! {
+    #[derive(Debug, EnumFromStr)]
+    enum UIType {
+        Terminal,
+        Gui,
+    }
+}
+
+#[derive(Debug)]
+enum UITypeError {
+    ParseEnumError(ParseEnumError, String),
+    VarError(VarError),
+}
+
+impl Display for UITypeError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            UITypeError::ParseEnumError(_, s) => write!(f, "Could not parse the UI type: {}", s),
+            UITypeError::VarError(e) => write!(f, "Error in getting env variable: {}", e),
+        }
+    }
+}
+
+impl Error for UITypeError {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        match self {
+            UITypeError::ParseEnumError(e, _) => Some(e),
+            UITypeError::VarError(e) => Some(e),
+        }
+    }
+}
 
 struct App {
     maze: Option<Rc<Maze>>,
@@ -33,12 +77,26 @@ impl App {
         }
     }
 
-    fn create_displayer(&self, gui: bool) -> Result<Box<dyn Displayer>, Box<dyn Error>> {
-        let mut displayer: Box<dyn Displayer> = if gui {
-            Box::new(GuiDisplayer::new())
-        } else {
-            Box::new(TerminalDisplayer::new(stdout()))
+    fn get_ui_type(&self) -> Result<UIType, UITypeError> {
+        match env::var("UI_TYPE") {
+            Ok(v) => v.parse().map_err(|e| UITypeError::ParseEnumError(e, v)),
+
+            Err(VarError::NotPresent) => Ok(UIType::Terminal),
+
+            e => e
+                .map(|_| UIType::Terminal) // needed to typecheck, but here we are always in error
+                .map_err(UITypeError::VarError),
+        }
+    }
+
+    fn create_displayer(&self) -> Result<Box<dyn Displayer>, Box<dyn Error>> {
+        let ui_type = self.get_ui_type()?;
+
+        let mut displayer: Box<dyn Displayer> = match ui_type {
+            UIType::Gui => Box::new(GuiDisplayer::new()),
+            UIType::Terminal => Box::new(TerminalDisplayer::default()),
         };
+
         displayer.init()?;
         Ok(displayer)
     }
@@ -52,7 +110,7 @@ impl App {
 
         let start_to_goal = heuristic.compute_heuristic(&maze.start);
 
-        let mut displayer = self.create_displayer(false)?;
+        let mut displayer = self.create_displayer()?;
 
         displayer.display_image(&maze, start_to_goal, None, None)?;
 
@@ -76,11 +134,11 @@ impl App {
         match res.0 {
             Some(path) => {
                 let (path, cost) = gen.reconstruct_path(&path);
+                displayer.display_image(&maze, start_to_goal, Some(&path), None)?;
                 println!("Path found!");
                 println!("Cost: {}", cost);
                 println!("Time: {}s", end_time.as_secs_f64());
                 println!("{:?}", res.1);
-                displayer.display_image(&maze, start_to_goal, Some(&path), None)?;
                 displayer.wait_for_end()?;
             }
             None => println!("Path not found"),
