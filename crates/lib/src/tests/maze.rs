4dc21b27c9e3806b281f616e54b8a5e237b6b233
Davide Canton <davide.canton5@gmail.com>
2024-08-24 14:45:51 +0200

    removed pub

--- crates/lib/src/tests/maze.rs
+++ crates/lib/src/tests/maze.rs
@@ -1,7 +1,7 @@
 use crate::{maze::Maze, position::Position};
 
 #[test]
-pub fn test_creation() {
+fn test_creation() {
     let maze = Maze::new(3, 4, Position::new(0, 0), Position::new(2, 3));
     assert_eq!(maze.width(), 3);
     assert_eq!(maze.height(), 4);
